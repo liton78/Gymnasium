9040c21d787543d7af5bff870f6e29818771c4c1
liton78 <91829066+liton78@users.noreply.github.com>
2021-12-09 08:44:10 +0300

    updeated

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ $(document).ready(function(){
     // venubox about
     $('.venobox').venobox();
 
+//     new Venobox({
+//     selector: '.venobox'
+// });
+
+
     // testimonial slider slick
 
     $('.testimonial-slider').slick({
@@ -17,6 +22,34 @@ $(document).ready(function(){
         arrows: false,
         slidesToShow: 2,
         slidesToScroll: 1,
+
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+        
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
     });
 
     // funfact counter
@@ -31,7 +64,41 @@ $(document).ready(function(){
         centerMode: true,
         centerPadding: '0px',
         prevArrow: '<i class="fas fa-chevron-left left-arrow"></i>',
-        nextArrow: '<i class="fas fa-chevron-right right-arrow"></i>'
+        nextArrow: '<i class="fas fa-chevron-right right-arrow"></i>',
+
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 5,
+                    infinite: true,
+                }
+            },
+        
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 3,
+                    autoplay:true
+                }
+            },
+            
+            {
+                breakpoint: 482,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    centerMode: false,
+                    autoplay:true
+                }
+            }
+        ],
+
+    });
+
+
+    $('.testimonial-2-slider').slick({
+        slidesToShow: 2,
     });
 
 });
