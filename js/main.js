f5c3b7c494dfbafdd37502139e4642f7452edf0e
liton78 <91829066+liton78@users.noreply.github.com>
2021-12-15 03:31:17 +0300

    updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
     'use strict'
+    // color switcher
+    $('.color-picker .color-btn').on('click',function(){
+        $('.color-picker').toggleClass('color-pos');
+    });
+
+    $('.color-picker ul .orange').on('click',function(){
+        $('body').addClass('orange').removeClass('green').removeClass('blue');
+    });
+
+    $('.color-picker ul .green').on('click',function(){
+        $('body').addClass('green').removeClass('orange').removeClass('blue');
+    });
+
+    $('.color-picker ul .blue').on('click',function(){
+        $('body').addClass('blue').removeClass('green').removeClass('orange');
+    });
+
+    $('.color-picker ul .difauld').on('click',function(){
+        $('body').addClass('difauld').removeClass('orange').removeClass('green').removeClass('blue');
+    });
+
     // for banner
     $('.banner-slider').slick({
         dots: true,
